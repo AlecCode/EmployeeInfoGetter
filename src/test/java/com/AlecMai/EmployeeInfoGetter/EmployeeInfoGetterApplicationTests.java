cf51e7c40172dbf1de9dccab74c31cebe36d9a95
Alec Mai <alec.z.mai@gmail.com>
2021-02-06 17:03:53 -0500

    Base setup

---
+++ src/test/java/com/AlecMai/EmployeeInfoGetter/EmployeeInfoGetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AlecMai.EmployeeInfoGetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeInfoGetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
